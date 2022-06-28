516e4e12f9095f4df20662a8d7a3ef8fe8385cf8
kelly1422 <kelly1422@naver.com>
2022-06-28 11:04:44 +0900

    [BOJ] 소셜 네트워킹 어플리케이션 / 골드5

---
+++ 8주차_UnionFind/[7511]소셜네트워킹어플리케이션.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] != x:
+        return find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a != b :
+        if a < b:
+            parent[b] = a
+        else :
+            parent[a] = b
+
+testcase = int(input())
+for i in range (testcase) :
+    print("Scenario ", end="")
+    print(i + 1, end="")
+    print(":")
+
+    user = int(input())
+    parent = {n: n for n in range(user)}
+
+    relation = int(input())
+    for j in range (relation) :
+        a, b = map(int, input().split())
+        union(a, b)
+    
+    find_num = int(input())
+    for j in range (find_num) :
+        u, v = map(int, input().split())
+        if find(u) == find(v) :
+            print(1)
+        else :
+            print(0)
+    if i != testcase - 1 :
+        print()
