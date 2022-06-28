ee9b5ba20860cf56456f1c142352ab855103d1aa
kelly1422 <kelly1422@naver.com>
2022-06-28 11:03:50 +0900

    [BOJ] 여러분의 다리가 되어 드리겠습니다! / 골드5

---
+++ 8주차_UnionFind/[17352]여러분의다리가되어드리겠습니다!.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] != x:
+        return find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a != b :
+        if a < b:
+            parent[b] = a
+        else :
+            parent[a] = b
+
+N = int(input())
+parent = {i: i for i in range(1, N + 1)}
+
+for _ in range(N - 2):
+    a, b = map(int, input().split())
+    union(a, b)
+
+group1 = find(1)
+for i in range(2, N + 1):
+    if group1 != find(i):
+        print(group1, i)
+        break
