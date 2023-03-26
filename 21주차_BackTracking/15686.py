6726bc13e8e87d1ddbd871de2c8b0152c485dde5
kelly1422 <kelly1422@naver.com>
2023-03-26 21:53:43 +0900

    [BOJ] 치킨 배달 / 골드5

---
+++ 21주차_BackTracking/15686.py
@@ -0,0 +1,32 @@
+import sys
+from itertools import combinations 
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+city = [list(map(int, input().split())) for _ in range (N)]
+
+homes = []
+chickens = []
+
+for i in range (N) :
+    for j in range (N) :
+        if city[i][j] == 1 :
+            homes.append((i, j))
+        elif city[i][j] == 2 :
+            chickens.append((i, j))
+
+
+def get_distance(homes, chickens) :
+    city_distance = 0
+    for home in homes :
+        distance = 1000
+        for chicken in chickens :
+            distance = min(distance, abs(home[0] - chicken[0]) + abs(home[1] - chicken[1]))
+        city_distance += distance
+    return city_distance
+
+result = 1000
+for comb in combinations(chickens, M) :
+    result = min(result, get_distance(homes, comb))
+    
+print(result)
