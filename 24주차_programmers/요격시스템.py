cc4a6f12dddea246ae5f98592d055540ad7cf1e2
kelly1422 <kelly1422@naver.com>
2023-05-09 23:33:34 +0900

    [Programmers] 요격 시스템 / lv2

---
+++ 24주차_programmers/요격시스템.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def solution(targets):
+    answer = 0
+    sort_t = sorted(targets, key= lambda x:x[1])
+
+    i = 0
+    while (i < len(sort_t)) :
+        print('i', i)
+        end = sort_t[i][1]
+        answer += 1
+        while (i < len(sort_t) - 1) :
+            if end > sort_t[i+1][0] :
+                i += 1
+            else :
+                break
+        i += 1
+    
+    return answer
+
+print(solution([[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]]))
